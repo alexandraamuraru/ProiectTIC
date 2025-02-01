da1b5f84de854bc03f7556433dfc1ddb0afeef95
unknown <alexandra.muraru.am@gmail.com>
2025-02-01 21:17:50 +0200

    Add routes on app.js file

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,11 @@ const logger = require('./src/config/logger-config');
 const { initializeFirebase } = require('./src/config/firebase-config');
 const errorHandler = require('./src/middlewares/error-handler');
 
+const authRoutes = require('./src/routes/auth-routes');
+const userRoutes = require('./src/routes/users-routes');
+const bookRoutes = require('./src/routes/books-routes');
+const loanRoutes = require('./src/routes/loans-routes');
+
 const app = express();
 
 app.use(cors());
@@ -13,8 +18,17 @@ app.use(express.urlencoded({ extended: true }));
 
 initializeFirebase();
 
+app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/books', bookRoutes);
+app.use('/api/loans', loanRoutes);
+
 app.get('/health', (req, res) => {
     res.status(200).json({ status: 'OK' });
+});
+
+app.use((req, res, next) => {
+    res.status(404).json({ message: 'Route not found' });
 });
 
 app.use(errorHandler);
